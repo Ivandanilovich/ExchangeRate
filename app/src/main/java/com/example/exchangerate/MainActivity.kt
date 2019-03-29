5bbc2b5effd423d1609a7da343d5a3346b52eb41
Ivan Danilovich <ivandanilovich99@gmail.com>
2019-03-29 11:22:17 +0700

    add map key

--- app/src/main/java/com/example/exchangerate/MainActivity.kt
+++ app/src/main/java/com/example/exchangerate/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.exchangerate
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
@@ -21,23 +22,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.i("df", "here")
+
+
         disposable = service.getLatest(getString(R.string.key), "RUB,USD")
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
                 { result ->
+                    Log.i("df", result.rates.RUB.toString())
                     eur.text = result.rates.RUB.toString()
                     usd.text = (result.rates.RUB / result.rates.USD).toString()
                     currentDate = reBuild(result.date)
                     date.text = currentDate
                 },
-                { error -> print("error") }
+                { e ->
+                    Log.e("error", "df")
+                }
             )
 
         RateLayout.setOnClickListener {
             val intent = Intent(this, RateActivity::class.java)
             intent.putExtra("date", currentDate)
             startActivity(intent)
+        }
+        banksLayout.setOnClickListener{
+            startActivity(Intent(this, MapsActivity::class.java))
         }
     }
 
